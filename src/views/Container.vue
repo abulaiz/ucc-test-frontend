58ed37f9046f50a303d0f981a985649a5d37eb70
abulaiz <ronabulaiz@gmail.com>
2020-10-10 07:40:24 +0700

    Starter View

---
+++ src/views/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span>Vehicle Tool App</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>  
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/views/Vehicle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="vehicle">
+    <v-row>
+    	<v-col cols="4">
+    		<v-card>
+    			<v-card-text>Create Vehicle</v-card-text>
+    		</v-card>
+    	</v-col>
+    	<v-col cols="8">
+    		<v-card>
+    			<v-card-text>List Vehicle</v-card-text>
+    		</v-card>
+    	</v-col>    	
+    </v-row>
+  </div>
+</template>
